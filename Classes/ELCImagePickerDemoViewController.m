024cecedf56cb2be6fbe7e0ca1ef07e75fd5c88d
Arup Saha <arup@zomato.com>
2013-03-12 15:03:29 +0530

    initial commit

--- Classes/ELCImagePickerDemoViewController.m
+++ Classes/ELCImagePickerDemoViewController.m
@@ -30,18 +30,28 @@
 
 #pragma mark ELCImagePickerControllerDelegate Methods
 
+NSMutableArray *dataObjects;
+
 - (void)elcImagePickerController:(ELCImagePickerController *)picker didFinishPickingMediaWithInfo:(NSArray *)info {
 	
 	[self dismissViewControllerAnimated:YES completion:^{
 	
+        dataObjects = [NSMutableArray arrayWithArray:info];
         for (UIView *v in [scrollview subviews]) {
             [v removeFromSuperview];
         }
     
         CGRect workingFrame = scrollview.frame;
         workingFrame.origin.x = 0;
+        
+        
+        ALAsset *dummy1 = (ALAsset *) [info lastObject];
+        ALAsset *dummy2 = (ALAsset *)[info objectAtIndex:0];
+        
+        [dataObjects insertObject:dummy1 atIndex:0];
+        [dataObjects addObject:dummy2];
 	
-        for(ALAsset *asset in info) {
+        for(ALAsset *asset in dataObjects) {
 	
             UIImageView *imageview = [[UIImageView alloc] initWithImage:[UIImage imageWithCGImage:[[asset defaultRepresentation] fullScreenImage]]];
             [imageview setContentMode:UIViewContentModeScaleAspectFit];
@@ -54,7 +64,10 @@
         }
 	
         [scrollview setPagingEnabled:YES];
+        
         [scrollview setContentSize:CGSizeMake(workingFrame.origin.x, workingFrame.size.height)];
+        
+        [scrollview scrollRectToVisible:CGRectOffset(scrollview.frame, workingFrame.size.width, 0) animated:NO];
         
     }];
 }
@@ -76,6 +89,16 @@
 	// e.g. self.myOutlet = nil;
 }
 
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if(scrollview.contentOffset.x == 0) {
+        NSLog(@"%@", @"we are at zero");
+        [scrollview scrollRectToVisible:CGRectOffset(scrollview.frame, (scrollview.contentSize.width - 2 * scrollview.frame.size.width), 0) animated:NO];
+    } else if(scrollview.contentOffset.x == (scrollview.contentSize.width - scrollview.frame.size.width)) {
+        NSLog(@"%@", @"we are at last");
+        [scrollview scrollRectToVisible:CGRectOffset(scrollview.frame, (scrollview.frame.size.width), 0) animated:NO];
+
+    }
+}
 
 - (void)dealloc {
     [super dealloc];
